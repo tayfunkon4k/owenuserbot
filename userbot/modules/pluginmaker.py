60d46a648848a812d01fddb6efb7b24b0b673374
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2021-12-01 10:49:50 +0300

    Update pluginmaker.py

--- userbot/modules/pluginmaker.py
+++ userbot/modules/pluginmaker.py
@@ -112,6 +112,6 @@ async def _(q):
 		return
 
 c_ = c("pluginmaker")
-c_.add_command("pmake", "<pluginin_komutu> <plugin_hızı> <edit/alt/foto/muzik> ", "@OwenUserBot Sizin İçin Bir plugin Oluşturur...")
-c_.add_info(' Thx to @ByMisakiMey').add()
+c_.add_command("pmake", "<pluginin_komutu> <plugin_hızı> <edit/alt/foto/muzik> ", "@SefillerUserBot Sizin İçin Bir plugin Oluşturur...")
+c_.add_info(' Thx to @TRCUMHURBASKANI').add()
 c_.add()
