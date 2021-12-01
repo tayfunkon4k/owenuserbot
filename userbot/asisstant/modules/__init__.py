76f5429d3494dfd426d551f80e3ae6ffc9f15556
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2021-12-01 10:57:42 +0300

    Update __init__.py

--- userbot/asisstant/modules/__init__.py
+++ userbot/asisstant/modules/__init__.py
@@ -4,7 +4,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-# OwenUserBot - ByMisakiMey - ErdewBey
+# SefillerUserBot - TRCUMHURBASKANI - SefillerOnlineTR
 
 
 from userbot import LOGS
