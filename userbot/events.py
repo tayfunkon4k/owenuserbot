7e6f3e7de814966fc3b274a7f7e3fabd95c76e1c
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2021-12-01 10:56:42 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - ErdewBey 
+# SefillerUserBot - TRCUMHURBASKANI 
 
 """ Olayları yönetmek için UserBot modülü.
  UserBot'un ana bileşenlerinden biri. """
