a57b4b368b6db122e7fe68a48ccc07bd3b90a567
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2021-12-01 10:28:25 +0300

    Update __epic.py

--- userbot/modules/__epic.py
+++ userbot/modules/__epic.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - ErdewBey - Midy - ByMisakiMey
+# SefillerUserBot - TRCUMHURBASKANI - SEFİLLER - SefillerOnlineTR
 #
 
 """ UserBot yardım komutu """
@@ -17,13 +17,13 @@ from userbot.events import register
 # ██████ LANGUAGE CONSTANTS ██████ #
 
 from userbot.language import get_value
-LANG = get_value("__owen")
+LANG = get_value("__sefiller")
 
 # ████████████████████████████████ #
 
-@register(outgoing=True, pattern="^.owen(?: |$)(.*)")
+@register(outgoing=True, pattern="^.sefiller(?: |$)(.*)")
 async def owen(event):
-    """ .owen komutun """
+    """ .sefiller komutun """
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
